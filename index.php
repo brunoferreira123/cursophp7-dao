62d00d80ee710f275d57e5254f23d8181f61b201
Bruno Martins <brunowebads@gmail.com>
2019-01-05 15:57:26 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
 	
 	require_once("config.php");
+	//carrega um usuario
+	//$root = new Usuario();
+	//$root->loadbyId(15);
 	
-	$root = new Usuario();
-	$root->loadbyId(15);
+	//echo $root;
 	
-	echo $root;
+	//carrega uma lista de usuarios
+		
+		//$lista = Usuario::getList();
+		//echo json_encode($lista);
+	
+	//Carrega uma lista de usuarios buscando pelo login
+	
+	//$search = Usuario::search("jo");
+	//echo json_encode($search);
+	
+	//Carrega um usuário usando o login e a senha
+	
+	$usuario = new Usuario();
+	$usuario->login("root","!@#$%");
+	echo $usuario;
 	/*
 	$sql = new Sql();
 	
